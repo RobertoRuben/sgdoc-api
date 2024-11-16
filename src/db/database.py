a8babb4520b46893b792cab8ba5562b800537157
chave <chave@CHVEZ05>
2024-11-16 17:04:14 -0500

    feat(areas): Se implemento el control de migraciones de la base de datos usando alembic creando una migracion inicial de las tablas actuales

--- src/db/database.py
+++ src/db/database.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, create_engine
 from src.config.settings import settings
+from src.model.entity import *
 
 postgres_url = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
 
---
+++ src/model/entity/__init__.py
@@ -0,0 +1,8 @@
+from .ambito import Ambito
+from .caserio import Caserio
+from .categoria import Categoria
+from .centro_poblado import CentroPoblado
+from .remitente import Remitente
+from .rol import Rol
+
+__all__ = ["Ambito", "Caserio", "Categoria", "CentroPoblado", "Remitente", "Rol"]
