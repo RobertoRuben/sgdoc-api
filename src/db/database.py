a96a34b6d3a7b444cb01bb0af8ffd8b92df54ef9
RobertoRuben <robertoch263@gmail.com>
2025-02-06 09:09:31 -0500

    ✨feat(database): implement asynchronous database engine and session management

--- src/db/database.py
+++ src/db/database.py
@@ -1,5 +1,7 @@
 from urllib.parse import quote_plus
 from sqlmodel import SQLModel, create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
+from sqlalchemy.orm import sessionmaker
 from src.config.settings import settings
 from src.model.entity import *
 
@@ -8,8 +10,16 @@ password = quote_plus(settings.POSTGRES_PASSWORD)
 
 postgres_url = f"postgresql://{user}:{password}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
 
-engine = create_engine(postgres_url, echo=True)
+engine = AsyncEngine(create_engine(postgres_url, echo=True, future=True))
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-    
+async def init_db():
+    async with engine.begin() as conn:
+        #await conn.run_sync(SQLModel.metadata.drop_all)
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(
+        engine, class_=AsyncSession, expire_on_commit=False
+    )
+    async with async_session() as session:
+        yield session
