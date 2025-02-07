2ab12f846b36cdcba2115f0d70ccbdcc523fa88d
RobertoRuben <robertoch263@gmail.com>
2025-02-06 20:55:33 -0500

    feat(controller): refactor CentroPobladoController to use CentroPobladoServiceImp and update method calls

--- src/controller/centro_poblado_controller.py
+++ src/controller/centro_poblado_controller.py
@@ -1,11 +1,11 @@
 from typing import List
-from fastapi import APIRouter, HTTPException, Depends, Query
+from fastapi import APIRouter, Depends, Query
 from fastapi.responses import JSONResponse
 from src.schemas import ErrorResponseSchema, ValidationErrorResponseSchema, NotAuthenticatedResponseSchema, DeleteSuccessfulResponseSchema
 from src.dto.centro_poblado_response_dto import CentroPobladoResponseDTO
 from src.dto.centro_poblado_request_dto import CentroPobladoRequestDTO
 from src.dto.paginated_response import PaginatedResponseDTO
-from src.service.centro_poblado_service import CentroPobladoService
+from src.service.imp.centro_poblado_service_imp import CentroPobladoServiceImp
 
 router = APIRouter(tags=["Centros Poblados"])
 
@@ -28,8 +28,8 @@ centros_poblados_tag_metadata={
     },
     description="Crea un nuevo centro poblado"
 )
-async def add_centro_poblado(centro_poblado_request: CentroPobladoRequestDTO, service: CentroPobladoService = Depends()):
-    return service.add_centro_poblado(centro_poblado_request)
+async def add_centro_poblado(centro_poblado_request: CentroPobladoRequestDTO, service: CentroPobladoServiceImp = Depends()):
+    return service.add(centro_poblado_request)
 
 
 @router.get(
@@ -42,8 +42,8 @@ async def add_centro_poblado(centro_poblado_request: CentroPobladoRequestDTO, se
     },
     description="Obtiene todos los centros poblados"
 )
-async def get_centros_poblados(service: CentroPobladoService = Depends()):
-    return service.get_all_centros_poblados()
+async def get_centros_poblados(service: CentroPobladoServiceImp = Depends()):
+    return service.get_all()
 
 
 @router.get(
@@ -59,9 +59,9 @@ async def get_centros_poblados(service: CentroPobladoService = Depends()):
 )
 async def search_centros_poblados(
     search_string: str = Query(..., min_length=1, description="Cadena de búsqueda para encontrar centros poblados"),
-    service: CentroPobladoService = Depends()
+    service: CentroPobladoServiceImp = Depends()
 ):
-    return service.find_centro_poblado_by_string(search_string)
+    return service.find(search_string)
 
 
 @router.get(
@@ -77,9 +77,9 @@ async def search_centros_poblados(
 async def get_paginated_centros_poblados(
     page: int = Query(1, description="Número de página a recuperar"),
     page_size: int = Query(10, description="Número de registros por página"),
-    service: CentroPobladoService = Depends()
+    service: CentroPobladoServiceImp = Depends()
 ):
-    return service.get_all_centros_poblados_paginated(page, page_size)
+    return service.get_paginated(page, page_size)
 
 
 @router.get(
@@ -92,8 +92,8 @@ async def get_paginated_centros_poblados(
         500: {"description": "Error interno del servidor", "model": ErrorResponseSchema},
     },
     description="Obtiene un centro poblado por su ID")
-async def get_centro_poblado_by_id(centro_poblado_id: int, service: CentroPobladoService = Depends()):
-    return service.get_centro_poblado_by_id(centro_poblado_id)
+async def get_centro_poblado_by_id(centro_poblado_id: int, service: CentroPobladoServiceImp = Depends()):
+    return service.get_by_id(centro_poblado_id)
 
 
 @router.put(
@@ -109,8 +109,8 @@ async def get_centro_poblado_by_id(centro_poblado_id: int, service: CentroPoblad
     },
     description="Actualiza un centro poblado"
 )
-async def update_centro_poblado(centro_poblado_id: int, centro_poblado_request: CentroPobladoRequestDTO, service: CentroPobladoService = Depends()):
-    return service.update_centro_poblado(centro_poblado_id, centro_poblado_request)
+async def update_centro_poblado(centro_poblado_id: int, centro_poblado_request: CentroPobladoRequestDTO, service: CentroPobladoServiceImp = Depends()):
+    return service.update(centro_poblado_id, centro_poblado_request)
 
 
 @router.delete(
@@ -124,6 +124,6 @@ async def update_centro_poblado(centro_poblado_id: int, centro_poblado_request: 
     },
     description="Elimina un centro poblado"
 )
-async def delete_centro_poblado(centro_poblado_id: int, service: CentroPobladoService = Depends()):
-    service.delete_centro_poblado_by_id(centro_poblado_id)
+async def delete_centro_poblado(centro_poblado_id: int, service: CentroPobladoServiceImp = Depends()):
+    service.delete_by_id(centro_poblado_id)
     return JSONResponse(content={"message": "Se eliminó el centro poblado correctamente"}, status_code=200)
--- src/service/__init__.py
+++ src/service/__init__.py
@@ -2,6 +2,8 @@ from .ambito_service import AmbitoService
 from .area_service import AreaService
 from .caserio_service import CaserioService
 from .categoria_documento_service import CategoriaDocumentoService
+from .centro_poblado_service import CentroPobladoService
 from .auth_service import AuthService
 
-__all__ = ["AmbitoService", "AreaService", "CaserioService", "CategoriaDocumentoService", "AuthService"]
+__all__ = ["AmbitoService", "AreaService", "CaserioService", "CategoriaDocumentoService", "AuthService",
+           "CentroPobladoService"]
