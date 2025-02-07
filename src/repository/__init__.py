322607744e722bb9536d1f037d928525b01559a6
RobertoRuben <robertoch263@gmail.com>
2025-02-06 20:42:13 -0500

    feat(repository): add CentroPobladoRepository to repository exports

--- src/repository/__init__.py
+++ src/repository/__init__.py
@@ -3,5 +3,7 @@ from .area_repository import AreaRepository
 from .caserio_repository import CaserioRepository
 from .categoria_documento_repository import CategoriaDocumentoRepository
 from .usuario_repository import UsuarioRepository
+from .centro_poblado_repository import CentroPobladoRepository
 
-__all__ = ["AmbitoRepository", "AreaRepository", "CaserioRepository", "CategoriaDocumentoRepository", "UsuarioRepository"]
+__all__ = ["AmbitoRepository", "AreaRepository", "CaserioRepository", "CategoriaDocumentoRepository",
+           "UsuarioRepository", "CentroPobladoRepository"]
