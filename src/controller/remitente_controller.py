5d050c94767c3e77c7f874e1170badf7faab8aac
RobertoRuben <robertoch263@gmail.com>
2025-02-07 14:39:00 -0500

    refactor(controller): update RemitenteController to use RemitenteService and improve dependency injection

--- src/controller/remitente_controller.py
+++ src/controller/remitente_controller.py
@@ -2,12 +2,14 @@ from typing import List
 from fastapi import APIRouter, Depends, Query
 from fastapi.responses import JSONResponse
 from src.schemas import *
-from src.service.imp.remitente_service_imp import RemitenteServiceImp
-from src.dto.remitente_request_dto import RemitenteRequestDTO
-from src.dto.remitente_response_dto import RemitenteResponseDTO
-from src.dto.paginated_response import PaginatedResponseDTO
+from src.dto import RemitenteRequestDTO, RemitenteResponseDTO, PaginatedResponseDTO
+from src.service import RemitenteService
+from src.service.imp import RemitenteServiceImp
 
-router = APIRouter(tags=["Remitentes"])
+router = APIRouter(
+    prefix="/remitentes",
+    tags=["Remitentes"]
+)
 
 remitentes_tag_metadata={
     "name": "Remitentes",
@@ -16,8 +18,11 @@ remitentes_tag_metadata={
                    " ofrece funcionalidades de paginación y conteo de registros.",
 }
 
+def get_remitentes_imp(service: RemitenteServiceImp = Depends()) -> RemitenteService:
+    return service
+
 @router.post(
-    "/remitentes",
+    "",
     response_model=RemitenteResponseDTO,
     responses={
         400: {"description": "Solicitud inválida", "model": ErrorResponseSchema},
@@ -28,12 +33,12 @@ remitentes_tag_metadata={
     },
     description="Crea un nuevo remitente"
 )
-async def add_remitente(remitente_request: RemitenteRequestDTO, service: RemitenteServiceImp = Depends()):
-    return service.add(remitente_request)
+async def add_remitente(remitente_request: RemitenteRequestDTO, service: RemitenteService = Depends(get_remitentes_imp)):
+    return await service.add(remitente_request)
 
 
 @router.get(
-    "/remitentes",
+    "",
     response_model=List[RemitenteResponseDTO],
     responses={
         400: {"description": "Solicitud inválida", "model": ErrorResponseSchema},
@@ -42,12 +47,12 @@ async def add_remitente(remitente_request: RemitenteRequestDTO, service: Remiten
     },
     description="Obtiene todos los remitentes"
 )
-async def get_remitentes(remitente_service: RemitenteServiceImp = Depends()):
-    return remitente_service.get_all()
+async def get_all_remitentes(service: RemitenteService = Depends(get_remitentes_imp)):
+    return await service.get_all()
 
 
 @router.get(
-    "/remitentes/search",
+    "/search",
     response_model=List[RemitenteResponseDTO],
     responses={
         400: {"description": "Solicitud inválida", "model": ErrorResponseSchema},
@@ -58,13 +63,13 @@ async def get_remitentes(remitente_service: RemitenteServiceImp = Depends()):
 )
 async def search_remitentes(
     search_string: str = Query(..., min_length=1, description="Cadena de búsqueda para encontrar remitentes"),
-    remitente_service: RemitenteServiceImp = Depends()
+    service: RemitenteService = Depends(get_remitentes_imp)
 ):
-    return remitente_service.find(search_string)
+    return await service.find(search_string)
 
 
 @router.get(
-    "/remitentes/paginated",
+    "/paginated",
     response_model=PaginatedResponseDTO,
     responses={
         400: {"description": "Solicitud inválida", "model": ErrorResponseSchema},
@@ -73,12 +78,16 @@ async def search_remitentes(
     },
     description="Obtiene la lista de remitentes paginada"
 )
-async def get_remitentes(page: int = 1, page_size: int = 10, remitente_service: RemitenteServiceImp = Depends()):
-    return remitente_service.get_paginated(page, page_size)
+async def get_paginated_remitentes(
+    page: int = 1,
+    page_size: int = 10,
+    service: RemitenteService = Depends(get_remitentes_imp)
+):
+    return await service.get_paginated(page, page_size)
 
 
 @router.get(
-    "/remitentes/{remitente_id}",
+    "/{remitente_id}",
     response_model=RemitenteResponseDTO,
     responses={
         400: {"description": "Solicitud inválida", "model": ErrorResponseSchema},
@@ -87,12 +96,15 @@ async def get_remitentes(page: int = 1, page_size: int = 10, remitente_service: 
     },
     description="Obtiene un remitente por ID"
 )
-async def get_remitente_by_id(remitente_id: int, remitente_service: RemitenteServiceImp = Depends()):
-    return remitente_service.get_by_id(remitente_id)
+async def get_remitente_by_id(
+    remitente_id: int,
+    service: RemitenteService = Depends(get_remitentes_imp)
+):
+    return await service.get_by_id(remitente_id)
 
 
 @router.put(
-    "/remitentes/{remitente_id}",
+    "/{remitente_id}",
     response_model=RemitenteResponseDTO,
     responses={
         400: {"description": "Solicitud inválida", "model": ErrorResponseSchema},
@@ -104,12 +116,16 @@ async def get_remitente_by_id(remitente_id: int, remitente_service: RemitenteSer
     },
     description="Actualiza un remitente"
 )
-async def update_remitente(remitente_id: int, remitente_request: RemitenteRequestDTO, remitente_service: RemitenteServiceImp = Depends()):
-    return remitente_service.update(remitente_id, remitente_request)
+async def update_remitente(
+    remitente_id: int,
+    remitente_request: RemitenteRequestDTO,
+    service: RemitenteService = Depends(get_remitentes_imp)
+):
+    return service.update(remitente_id, remitente_request)
 
 
 @router.delete(
-    "/remitentes/{remitente_id}",
+    "/{remitente_id}",
     response_model=DeleteSuccessfulResponseSchema,
     responses={
         400: {"description": "Solicitud inválida", "model": ErrorResponseSchema},
@@ -119,9 +135,15 @@ async def update_remitente(remitente_id: int, remitente_request: RemitenteReques
     },
     description="Elimina un remitente"
 )
-async def delete_remitente(remitente_id: int, remitente_service: RemitenteServiceImp = Depends()):
-    remitente_service.delete_by_id(remitente_id)
-    return JSONResponse(content={"message": "Se eliminó el remitente correctamente"}, status_code=200)
+async def delete_remitente_by_id(
+    remitente_id: int,
+    service: RemitenteService = Depends(get_remitentes_imp)
+):
+    await service.delete_by_id(remitente_id)
+    return JSONResponse(
+        content={"message": "Se eliminó el remitente correctamente"},
+        status_code=200
+    )
 
 
 
