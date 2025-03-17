9ebb7fe009a214430cce1a4701f651f7ac4b1f02
RobertoRuben <robertoch263@gmail.com>
2025-03-17 16:39:29 -0500

    feat(comunicacion): refactor validation logic in request DTOs and update response DTO structure

--- src/dto/comunicacion_area_response_dto.py
+++ src/dto/comunicacion_area_response_dto.py
@@ -1,11 +1,19 @@
 from pydantic import BaseModel
 
-class ComunicacionAreaResponseDTO(BaseModel):
+class ComunicacionAreaSimpleResponseDTO(BaseModel):
     id: int
-    area_origen_nombre: str
-    area_destino_nombre: str
+    area_origen_id: int | None = None
+    area_destino_id: int | None = None
+    area_origen_nombre: str | None = None
+    area_destino_nombre: str | None = None
 
 
 class ComunicacionDestinoResponseDTO(BaseModel):
     area_destino_id: int
+    nombre_area_destino: str
+
+
+class ComunicacionAreaFindResponseDTO(BaseModel):
+    id: int
+    nombre_area_origen: str
     nombre_area_destino: str
